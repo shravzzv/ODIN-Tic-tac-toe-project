5ef22f28e3d3498ab2e0651c392a44e44b4cda5b
sai shravan <saishravan384@gmail.com>
2023-09-05 15:01:21 +0530

    refactor: Tuck game logic inside game module

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-const gameBoardEl = document.querySelector('.gameBoard')
-const tileEls = document.querySelectorAll('.tile')
+// module
+const game = (function () {
+  const tileEls = document.querySelectorAll('.tile')
+
+  const start = () => {
+    let invertTurn = false
+
+    const handleTileClick = (e) => {
+      if (e.target.textContent !== '') return
+      invertTurn ? player2.makeMove() : player1.makeMove()
+      e.target.textContent = gameboard.getLastMove()
+      invertTurn = !invertTurn
+    }
+
+    Array.from(tileEls).forEach((tile) =>
+      tile.addEventListener('click', handleTileClick)
+    )
+  }
+
+  const end = () => {}
+
+  return { start, end }
+})()
 
 // module
-const Gameboard = (function GameBoard() {
+const gameboard = (function GameBoard() {
   const board = []
   const addMove = (move) => board.push(move)
-
-  return { board, addMove }
+  const getBoardLength = () => board.length
+  const getLastMove = () => board[getBoardLength() - 1]
+  return { addMove, getBoardLength, getLastMove }
 })()
 
 // factory
@@ -14,7 +36,7 @@ const Player = (name, choice) => {
   const getName = () => name
   const getChoice = () => choice
   const makeMove = () => {
-    Gameboard.addMove(choice)
+    gameboard.addMove(choice)
   }
   return { getName, getChoice, makeMove }
 }
@@ -22,15 +44,4 @@ const Player = (name, choice) => {
 const player1 = Player('jeff', 'X')
 const player2 = Player('samantha', 'O')
 
-let invertTurn = false
-
-const handleTileClick = (e) => {
-  if (e.target.textContent !== '') return
-  invertTurn ? player2.makeMove() : player1.makeMove()
-  e.target.textContent = Gameboard.board[Gameboard.board.length - 1]
-  invertTurn = !invertTurn
-}
-
-Array.from(tileEls).forEach((tile) =>
-  tile.addEventListener('click', handleTileClick)
-)
+if (player1 && player2) game.start()
