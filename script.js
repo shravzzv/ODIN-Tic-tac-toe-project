99ca0afc5a2ba804db242f061a25ca002d74ff7e
sai shravan <saishravan384@gmail.com>
2023-09-05 13:14:41 +0530

    feat: Create Gameboard module & Player factory

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hello world')
+// module
+const Gameboard = (function GameBoard() {
+  const board = []
+  const addMove = (move) => board.push(move)
+
+  return { board, addMove }
+})()
+
+// factory
+const Player = (name, choice) => {
+  const getName = () => name
+  const getChoice = () => choice
+  const makeMove = () => {
+    Gameboard.addMove(choice)
+  }
+  return { getName, getChoice, makeMove }
+}
+
+const player1 = Player('jeff', 'X')
+const player2 = Player('samantha', 'O')
+
+player1.makeMove()
+player2.makeMove()
+console.log(Gameboard)
