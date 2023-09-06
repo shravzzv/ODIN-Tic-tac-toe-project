7f1122e3208051d44ccd00817ded6501c690bc00
sai shravan <saishravan384@gmail.com>
2023-09-06 13:25:24 +0530

    refactor: Update modules to make _board more specific

--- script.js
+++ script.js
@@ -1,42 +1,84 @@
 // module
 const game = (function () {
-  const _tileEls = document.querySelectorAll('.tile')
+  const _tileElsArr = Array.from(document.querySelectorAll('.tile'))
 
   const start = () => {
+    gameboard.populateBoard(_tileElsArr)
     let _invertTurn = false
 
     const _handleTileClick = (e) => {
-      if (e.target.textContent !== '') return
-      _invertTurn ? player2.makeMove() : player1.makeMove()
-      e.target.textContent = gameboard.getLastMove()
+      if (e.currentTarget.textContent !== '') return
+      _invertTurn
+        ? player2.makeMove(_tileElsArr.indexOf(e.currentTarget))
+        : player1.makeMove(_tileElsArr.indexOf(e.currentTarget))
+      gameboard.populateBoard(_tileElsArr)
       _invertTurn = !_invertTurn
     }
 
-    Array.from(_tileEls).forEach((tile) =>
+    _tileElsArr.forEach((tile) =>
       tile.addEventListener('click', _handleTileClick)
     )
   }
 
-  const end = () => {}
+  const end = () => {
+    const result = gameboard.checkEndGame()
+    if (result === 'X' || result === 'O') {
+      // Display a message for the winner
+    } else if (result === 'draw') {
+      // Display a message for a draw
+    }
+    // Remove event listeners here if the game is over
+  }
 
   return { start, end }
 })()
 
 // module
 const gameboard = (function GameBoard() {
-  const _board = []
-  const addMove = (move) => _board.push(move)
-  const getBoardLength = () => _board.length
-  const getLastMove = () => _board[getBoardLength() - 1]
-  return { addMove, getBoardLength, getLastMove }
+  const _board = ['', '', '', '', '', '', '', '', '']
+  const _winningCombinations = [
+    // rows
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagnols
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  const checkEndGame = () => {
+    for (const combination of _winningCombinations) {
+      let [a, b, c] = combination
+      if (_board[a] === 'X' && _board[b] === 'X' && _board[c] === 'X') {
+        console.log('X is the winner')
+      }
+    }
+  }
+
+  const populateBoard = (tiles) => {
+    tiles.forEach((tile, i) => (tile.textContent = _board[i]))
+  }
+
+  const updateBoard = (move, index) => {
+    _board.splice(index, 1, move)
+    console.log(_board)
+  }
+
+  const getItem = (index) => _board[index]
+
+  return { updateBoard, checkEndGame, populateBoard, getItem }
 })()
 
 // factory
 const Player = (name, choice) => {
   const getName = () => name
   const getChoice = () => choice
-  const makeMove = () => {
-    gameboard.addMove(choice)
+  const makeMove = (index) => {
+    gameboard.updateBoard(choice, index)
   }
   return { getName, getChoice, makeMove }
 }
@@ -44,4 +86,4 @@ const Player = (name, choice) => {
 const player1 = Player('jeff', 'X')
 const player2 = Player('samantha', 'O')
 
-if (player1 && player2) game.start()
+game.start()
